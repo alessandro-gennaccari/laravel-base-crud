d761cbb75a4460ddc1af3f44a1440c309c2de602
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-17 15:08:53 +0100

    added_navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,21 @@
 <header>
 
-    
+    <nav class="navbar navbar-expand-lg navbar-light bg-info text-uppercase font-weight-bold">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Film</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
 </header>
