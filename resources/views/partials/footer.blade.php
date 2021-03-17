6678a02079005ebd5889db92f176fd0434189e35
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-17 16:12:16 +0100

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,13 @@
-<footer>
+<footer class="fixed-bottom">
 
-    
+    <div class="text-center text-dark bg-secondary font-weight-bold">
+        <div class="card-header">
+            Credits
+        </div>
+        <div class="card-body">
+            <h5 class="card-title font-weight-bold">I dettagli dei tuoi film in un unico sito</h5>
+            <p class="card-text">Testo indicativo per espimere qualcosa, un po lungo ma non troppo</p>
+        </div>
+    </div>
 
 </footer>
