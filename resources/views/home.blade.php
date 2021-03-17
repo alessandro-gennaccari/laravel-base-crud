e514fc84116f0719845c381a111b82b479f7e8c6
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-17 15:09:07 +0100

    added h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,10 @@
 @section('title', 'Homepage')
     
 @section('content')
-    <main>
+    <main class="p-5">
 
-        
+        <h1 class="text-center">Tutto sui tuoi film preferiti</h1>
+
 
     </main>
 @endsection
