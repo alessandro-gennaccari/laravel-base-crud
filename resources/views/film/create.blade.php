8e0a67d1bef745672f40c66d54495a69d40dfcd3
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-18 13:34:37 +0100

    added form

--- resources/views/film/create.blade.php
+++ resources/views/film/create.blade.php
@@ -3,7 +3,30 @@
 @section('title', 'Agginta nuovo film')
     
 @section('content')
-<main>
-    
+<main class="p-5">
+
+    <h1 class="text-center mb-5">Aggiunta film</h1>
+
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="inputname" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="inputname">
+            </div>
+            <div class="mb-3">
+                <label for="inputgenre" class="form-label">Genere</label>
+                <input type="text" class="form-control" id="inputgenre">
+            </div>
+            <div class="mb-3">
+                <label for="inputdirector" class="form-label">Regia</label>
+                <input type="text" class="form-control" id="inputdirector">
+            </div>
+            <div class="mb-3">
+                <label for="inputyear" class="form-label">Anno</label>
+                <input type="text" class="form-control" id="inputyear" placeholder="YYYY">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 </main>
 @endsection
