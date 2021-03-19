f7eaae9d56dbabc37ace17e7138440e87b7eb7e4
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-19 15:17:23 +0100

    prepopulate input data after validation failed

--- resources/views/film/create.blade.php
+++ resources/views/film/create.blade.php
@@ -22,19 +22,19 @@
             @method('POST')
             <div class="mb-3">
                 <label for="inputname" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="inputname" name="name">
+                <input type="text" class="form-control" id="inputname" name="name" value="{{ old('name') }}">
             </div>
             <div class="mb-3">
                 <label for="inputgenre" class="form-label">Genere</label>
-                <input type="text" class="form-control" id="inputgenre" name="genre">
+                <input type="text" class="form-control" id="inputgenre" name="genre" value="{{ old('genre') }}">
             </div>
             <div class="mb-3">
                 <label for="inputdirector" class="form-label">Regia</label>
-                <input type="text" class="form-control" id="inputdirector" name="director">
+                <input type="text" class="form-control" id="inputdirector" name="director" value="{{ old('director') }}">
             </div>
             <div class="mb-5">
                 <label for="inputyear" class="form-label">Anno</label>
-                <input type="text" class="form-control" id="inputyear" placeholder="YYYY" name="year">
+                <input type="text" class="form-control" id="inputyear" placeholder="YYYY" name="year" value="{{ old('year') }}">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
